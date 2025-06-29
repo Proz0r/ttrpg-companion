ece39adb448fc1ba47df20a4830259f0f93f8b82
Proz0r <mcboboby7@gmail.com>
2025-06-30 00:17:17 +1000

    Update CORS configuration and deployment scripts

--- server/index.js
+++ server/index.js
@@ -7,6 +7,14 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 3001;
 
+// Add CORS headers
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+
 // Serve static files from the build directory
 app.use(express.static(path.join(__dirname, '../client/build')));
 
@@ -18,6 +26,14 @@ app.get('/', (req, res) => {
 // Add catch-all route for React Router
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+});
+
+// Handle preflight requests
+app.options('*', (req, res) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.status(200).end();
 });
 
 // Middleware
